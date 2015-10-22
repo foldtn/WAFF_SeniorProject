10b7d566b0c30b5336283075e8acc2d42c55c86a
Timothy Folds <n00830864@unf.edu>
2015-10-22 16:55:29 -0400

    changes

---
+++ WAFF.WebUI/Models/Block.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WAFF.WebUI.Models
+{
+    public class Block
+    {
+        public int BlockID { get; set; }
+        public DateTime BlockStart { get; set; }
+        public DateTime BlockEnd { get; set; }
+        public string BlockLocation { get; set; }
+        public string BlockType { get; set; }
+        public int EventID { get; set; }
+
+    }
+}
---
+++ WAFF.WebUI/Models/FilmArtist.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WAFF.WebUI.Models
+{
+    public class FilmArtist
+    {
+        public int FilmID { get; set; }
+        public int ArtistID { get; set; }
+
+    }
+}
