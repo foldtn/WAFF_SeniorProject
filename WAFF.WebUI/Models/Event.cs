3402239420d0d1b482a0ac4170cde4e0e26e05b2
Jeremiah Doody <n00810032@ospreys.unf.edu>
2015-10-22 07:29:15 -0400

    Added Event Model

---
+++ WAFF.WebUI/Models/Event.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WAFF.WebUI.Models
+{
+    public class Event
+    {
+        public int Event_ID { get; set; }
+        public DateTime Event_StartDate { get; set; }
+        public DateTime Event_EndDate { get; set; }
+        public string Event_Location { get; set; }
+    }//end class
+}//end namespace
