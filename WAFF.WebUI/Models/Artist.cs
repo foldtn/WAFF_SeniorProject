b8cddc0bba9621cc42273684d1dc6c2fe7393bcc
John Verdi <john@clrchoice.com>
2015-10-22 10:57:24 -0400

    reverted initial commit

---
+++ WAFF.WebUI/Models/Artist.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WAFF.WebUI.Models
+{
+    public class Artist
+    {
+        public int Artist_Id { get; set; }
+        public string Artist_FName { get; set; }
+        public string Artist_LName { get; set; }
+        public string Artist_Company { get; set; }
+        public string Artist_Email { get; set; }
+        public string Artist_Address { get; set; }
+        public string Artist_City { get; set; }
+        public string Artist_State { get; set; }
+        public string Artist_Zip { get; set; }
+        public string Artist_Phone { get; set; }
+    }
+}
