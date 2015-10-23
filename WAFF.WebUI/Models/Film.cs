79c25e8dab3ad6946d09d3cc2a296b4eaab28134
John Verdi <john@clrchoice.com>
2015-10-23 17:17:03 -0400

    Created Vote class

---
+++ WAFF.WebUI/Models/Film.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WAFF.WebUI.Models
+{
+    public class Film
+    {
+    public int VoteId { get; set; }
+    public string VoteComment { get; set; }
+    public DateTime VoteTimeStamp { get; set; }
+    public int VoterId { get; set; }
+    public int FilmId { get; set; }
+    }
+}
