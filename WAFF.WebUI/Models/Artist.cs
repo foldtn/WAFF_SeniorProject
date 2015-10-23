188994fbb20290a106fb1f495a512d412a35724b
John Verdi <john@clrchoice.com>
2015-10-23 17:37:13 -0400

    Built Artist and Film classes

---
+++ WAFF.WebUI/Models/Artist.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WAFF.WebUI.Models
+{
+    public class Artist
+    {
+        public int ArtistId { get; set; }
+        public string ArtistFName { get; set; }
+        public string ArtistLName { get; set; }
+        public string ArtistCompany { get; set; }
+        public string ArtistEmail { get; set; }
+        public string ArtistAddress { get; set; }
+        public string ArtistCity { get; set; }
+        public string ArtistState { get; set; }
+        public int ArtistZip { get; set; }
+        public string ArtistPhone { get; set; }
+    }
+}
