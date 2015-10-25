3bfe4df790b81fb05a8b1b386b49ad685a7b9e3c
MegaJuly86 <jorozco185@gmail.com>
2015-10-25 19:56:26 -0400

    Create table Voter with attributes

---
+++ WAFF_dbProject/dbo/tables/Voter.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[test]
+(
+	[ID] INT NOT NULL PRIMARY KEY, 
+    [Age] NCHAR(10) NOT NULL, 
+    [Ethnicity] VARCHAR(50) NOT NULL, 
+    [Education] VARCHAR(50) NOT NULL, 
+    [Income] MONEY NOT NULL
+)
