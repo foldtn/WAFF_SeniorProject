c3d7a5aeacfdf5ea0de1e5fe0e335b3edd2a4842
MegaJuly86 <jorozco185@gmail.com>
2015-11-01 12:12:00 -0500

    Create QRCode Attribute

--- WAFF_dbProject/dbo/tables/Voter.sql
+++ WAFF_dbProject/dbo/tables/Voter.sql
@@ -1,8 +1,9 @@
 ﻿CREATE TABLE [dbo].[test]
 (
-	[ID] INT NOT NULL PRIMARY KEY, 
-    [Age] NCHAR(10) NOT NULL, 
-    [Ethnicity] VARCHAR(50) NOT NULL, 
-    [Education] VARCHAR(50) NOT NULL, 
-    [Income] MONEY NOT NULL
+	[VoterID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [VoterAge] NCHAR(10) NOT NULL, 
+    [VoterEthnicity] VARCHAR(50) NOT NULL, 
+    [VoterEducation] VARCHAR(50) NOT NULL, 
+    [VoterIncome] MONEY NOT NULL, 
+    [VoterQRCode] NVARCHAR(200) NOT NULL
 )
