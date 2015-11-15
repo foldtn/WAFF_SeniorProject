4a2ac4747c1f99257549e7bac8d523ee3b8a3d01
Timothy Folds <n00830864@unf.edu>
2015-11-15 16:58:26 -0500

    EFDbContext file, attempting to fix WAFF_DBProject...

---
+++ WAFF.WebUI/Models/EFDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace WAFF.WebUI.Models
+{
+    public class EFDbContext : DbContext
+    {
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Block> Blocks { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Film> Films { get; set; }
+        public DbSet<FilmArtist> FilmArtists { get; set; }
+        public DbSet<Vote> Votes { get; set; }
+        public DbSet<Voter> Voters { get; set; }
+    }
+}
