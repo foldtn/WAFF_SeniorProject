730077d1ca90021924c6e8e8975fa194b5148d84
Timothy Folds <n00830864@unf.edu>
2015-11-16 13:27:12 -0500

    Created a Reports view to generate the layout view, installed bootstrap package

--- WAFF.WebUI/Controllers/ReportsController.cs
+++ WAFF.WebUI/Controllers/ReportsController.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WAFF.Services.Reports;
 
 namespace WAFF.WebUI.Controllers
 {
     public class ReportsController : Controller
     {
-        // GET: Reports
+        
         public ActionResult Reports()
         {
             return View();
