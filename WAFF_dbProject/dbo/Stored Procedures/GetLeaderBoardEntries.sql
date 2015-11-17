f5e2db235434692a9687a75bd33876227dd0bf19
Timothy Folds <n00830864@unf.edu>
2015-11-17 16:20:40 -0500

    drawing a fucking owl

---
+++ WAFF_dbProject/dbo/Stored Procedures/GetLeaderBoardEntries.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE [dbo].[GetLeaderBoardEntries]
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT
+		f.FilmName,
+		f.FilmGenre,
+		f.BlockID,
+		COUNT(v.FilmID) AS [Votes]
+
+	FROM [FILMS] f
+
+	JOIN [VOTES] v 
+		ON f.FilmID = v.FilmID
+
+	GROUP BY
+		f.FilmName,
+		f.FilmGenre,
+		f.BlockID
+END
