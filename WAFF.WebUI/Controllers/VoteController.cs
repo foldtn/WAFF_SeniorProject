604a63b31770d162cf4da091ae6feeedeb94978b
MegaJuly86 <jorozco185@gmail.com>
2015-11-21 17:31:28 -0500

    Posting back to the server from view

--- WAFF.WebUI/Controllers/VoteController.cs
+++ WAFF.WebUI/Controllers/VoteController.cs
@@ -4,21 +4,30 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WAFF.DataAccess.Entity;
+using WAFF.Services.Votes;
 
 namespace WAFF.WebUI.Controllers
 {
     public class VoteController : Controller
     {
- 
+        private VoteService _service = new VoteService();
         // GET: Vote
         [HttpGet]
-        public ActionResult Vote(int VoterID)
+        public ActionResult Vote(int id)
         {
-            return View();
+            var blockId = 1;
+
+            var model = new Vote
+            {
+                VoterID = id,
+                BlockID = blockId,
+            };
+            return View(model);
         }
         [HttpPost]
         public ActionResult Vote(Vote vote )
         {
+            _service.SaveVote(vote);
             return View();
         }
     }
