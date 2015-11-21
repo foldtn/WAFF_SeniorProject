a151476a0ecec03f58d6fb6b759b9e935673c3ee
MegaJuly86 <jorozco185@gmail.com>
2015-11-21 15:57:57 -0500

    Create view,dbcontext, add service layer

--- WAFF.Services/Votes/VoteService.cs
+++ WAFF.Services/Votes/VoteService.cs
@@ -4,11 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WAFF.DataAccess.Contexts;
+using WAFF.DataAccess.Entity;
 
 namespace WAFF.Services.Votes
 {
     public class VoteService
     {
+        private EFDbContext _db = new EFDbContext();
 
+
+        public void SaveVote(Vote vote)
+        {
+            _db.Votes.Add(vote);
+            _db.SaveChanges();
+        }
     }
+
+    
 }
---
+++ WAFF.WebUI/Controllers/VoteController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WAFF.DataAccess.Entity;
+
+namespace WAFF.WebUI.Controllers
+{
+    public class VoteController : Controller
+    {
+ 
+        // GET: Vote
+        [HttpGet]
+        public ActionResult Vote(int VoterID)
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Vote(Vote vote )
+        {
+            return View();
+        }
+    }
+}
