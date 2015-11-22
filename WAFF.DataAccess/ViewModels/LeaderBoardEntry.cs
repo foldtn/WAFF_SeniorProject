02f08578183fb2d0159365f7cdf65f75863ac034
Timothy Folds <n00830864@unf.edu>
2015-11-22 02:03:10 -0500

    Help... I can't... control... the urge... to work...

--- WAFF.DataAccess/ViewModels/LeaderBoardEntry.cs
+++ WAFF.DataAccess/ViewModels/LeaderBoardEntry.cs
@@ -11,6 +11,9 @@ namespace WAFF.DataAccess.ViewModels
         public string FilmName { get; set; }
         public string FilmGenre { get; set; }
         public int BlockID { get; set; }
+        public string BlockStart { get; set; }
+        public string BlockEnd { get; set; }
+        public string BlockDayOfWeek { get; set; }
         public decimal VotePercentage { get; set; }
     }
 }
