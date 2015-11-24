a662aaddb42c9c4d0d1c41f6464e13442728e5a3
John Verdi <john@clrchoice.com>
2015-11-24 11:24:29 -0500

    Modified labels to be more intuitive

--- WAFF.DataAccess/Entity/Artist.cs
+++ WAFF.DataAccess/Entity/Artist.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -7,15 +8,25 @@ namespace WAFF.DataAccess.Entity
 {
     public class Artist
     {
+        
         public int ArtistID { get; set; }
+        [DisplayName("Artist's First Name")]
         public string ArtistFName { get; set; }
+        [DisplayName("Last Name")]
         public string ArtistLName { get; set; }
+        [DisplayName("Company")]
         public string ArtistCompany { get; set; }
+        [DisplayName("Email")]
         public string ArtistEmail { get; set; }
+        [DisplayName("Address")]
         public string ArtistAddress { get; set; }
+        [DisplayName("City")]
         public string ArtistCity { get; set; }
+        [DisplayName("State")]
         public string ArtistState { get; set; }
+        [DisplayName("Zip Code")]
         public int ArtistZip { get; set; }
+        [DisplayName("Phone")]
         public string ArtistPhone { get; set; }
     }
 }
--- WAFF.DataAccess/Entity/Block.cs
+++ WAFF.DataAccess/Entity/Block.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,13 @@ namespace WAFF.DataAccess.Entity
     public class Block
     {
         public int BlockID { get; set; }
+        [DisplayName("Start Time")]
         public DateTime BlockStart { get; set; }
+        [DisplayName("End Time")]
         public DateTime BlockEnd { get; set; }
+        [DisplayName("Location")]
         public string BlockLocation { get; set; }
+        [DisplayName("Type")]
         public string BlockType { get; set; }
         public int EventID { get; set; }
 
