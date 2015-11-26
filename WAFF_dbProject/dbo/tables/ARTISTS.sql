5a035ab8bd8f41f24447a01bdbca5684c8ae7088
John Verdi <john@clrchoice.com>
2015-11-26 11:01:19 -0500

    Allow Nulls in Artist Table

--- WAFF_dbProject/dbo/tables/ARTISTS.sql
+++ WAFF_dbProject/dbo/tables/ARTISTS.sql
@@ -3,11 +3,11 @@
 	[ArtistID] INT NOT NULL PRIMARY KEY IDENTITY, 
     [ArtistFName] VARCHAR(30) NOT NULL, 
     [ArtistLName] VARCHAR(30) NOT NULL, 
-    [ArtistCompany] VARCHAR(30) NOT NULL,
-	[ArtistEmail] VARCHAR(30) NOT NULL,
-	[ArtistAddress] VARCHAR(50) NOT NULL,
-	[ArtistCity] VARCHAR(30) NOT NULL,
-	[ArtistState] VARCHAR(2) NOT NULL,
-	[ArtistZip] INT NOT NULL,
-	[ArtistPhone] VARCHAR(30) NOT NULL
+    [ArtistCompany] VARCHAR(30) NULL,
+	[ArtistEmail] VARCHAR(30) NULL,
+	[ArtistAddress] VARCHAR(50) NULL,
+	[ArtistCity] VARCHAR(30) NULL,
+	[ArtistState] VARCHAR(2) NULL,
+	[ArtistZip] INT NULL,
+	[ArtistPhone] VARCHAR(30) NULL
 )
