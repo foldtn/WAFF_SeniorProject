bc269324c0fb595d70715cea8f440ab7db848a7c
Timothy Folds <n00830864@unf.edu>
2015-12-04 00:03:29 -0500

    local changes

--- WAFF.WebUI/Scripts/Custom/LeaderBoardsFullScreen.js
+++ WAFF.WebUI/Scripts/Custom/LeaderBoardsFullScreen.js
@@ -3,16 +3,18 @@
         enterFull(docElm);
         document.getElementById("isFull").value = "yes";
         document.getElementById("mainNav").style.display = "none";
-        document.getElementById("reportsNav").style.display = "none";
+        //document.getElementById("reportsNav").style.display = "none";
         document.getElementById("footer").style.display = "none";
+        document.getElementById("girlImageLayout").style.display = "none"
         document.getElementById("toFull").innerHTML = "Exit Fullscreen";
     }
     else {
         exitFull();
         document.getElementById("isFull").value = "no";
         document.getElementById("mainNav").style.display = "inline";
-        document.getElementById("reportsNav").style.display = "inline";
+        //document.getElementById("reportsNav").style.display = "inline";
         document.getElementById("footer").style.display = "inline";
+        document.getElementById("girlImageLayout").style.display = "inline";
         document.getElementById("toFull").innerHTML = "Enter Fullscreen";
     }
 }
