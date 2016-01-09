59862faef0ebf6d925f5026c5de60921960e828c
Timothy Folds <n00830864@unf.edu>
2016-01-09 00:37:51 -0500

    fixed getFilmsB.sql to account for database changes

--- WAFF_dbProject/dbo/Stored Procedures/getFilmsB.sql
+++ WAFF_dbProject/dbo/Stored Procedures/getFilmsB.sql
@@ -1,1 +1,12 @@
-﻿
+﻿CREATE PROCEDURE [dbo].[getFilmsB]
+	@block int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+  SELECT f.FilmID, f.FilmName 
+  FROM [FILMS] f
+  JOIN [FILMBLOCKS] fb
+  ON f.FilmID = fb.FilmID
+  WHERE fb.BlockID = @block
+END
