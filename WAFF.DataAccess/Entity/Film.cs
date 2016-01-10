407f99781a02cd1290661fec226945c0e7b9d8bb
John Verdi <john@clrchoice.com>
2016-01-09 22:21:30 -0500

    Used metadata at model level to help generate views, see Film Edit.

--- WAFF.DataAccess/Entity/Film.cs
+++ WAFF.DataAccess/Entity/Film.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace WAFF.DataAccess.Entity
 {
     public class Film
     {
+        [HiddenInput(DisplayValue = false)]
         public int FilmID { get; set; }
         [DisplayName("Film Name")]        
         public string FilmName { get; set; }
         [DisplayName("Genre")]
         public string FilmGenre { get; set; }
         [DisplayName("Description")]
+        [DataType(DataType.MultilineText)]
         public string FilmDesc { get; set; }
         [DisplayName("Length (minutes)")]
         public int FilmLength { get; set; }
