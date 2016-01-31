5c55e335d5ca1c275be9e0a70ab0fabca3e5cc9b
John Verdi <john@clrchoice.com>
2016-01-31 03:25:26 -0500

    Revert "removed 'winners.cs' model (it was from early last term and I forgot to remove it)"
    This reverts commit 0c5b4a495a503d65c5a866b82cce80f532d49c4c.

---
+++ WAFF.WebUI/Models/Winners.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WAFF.WebUI.Models
+{
+    public class Winners
+    {
+        public string FilmName { get; set; }
+        public string FilmGenre { get; set; }
+        public int BlockID { get; set; }
+        public double VotePercent { get; set; }
+    }
+}
