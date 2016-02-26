92708dbeead4edc292027116670b5c9f0de62f44
Timothy Folds <n00830864@ospreys.unf.edu>
2016-02-26 15:16:26 -0500

    2-3 more times...

---
+++ WAFF_dbProject/dbo/Stored Procedures/getLBVoted.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[getLBVoted]
+	@event int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT DISTINCT v.FilmID, v.BlockID
+	FROM VOTES v
+	JOIN BLOCKS b
+	ON v.BlockID = b.BlockID
+	WHERE b.EventID = @event
+
+END
