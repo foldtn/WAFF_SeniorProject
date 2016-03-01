9b74e55620dbdac188397dcedca389d1fa2698cd
John Verdi <john@clrchoice.com>
2016-03-01 18:26:42 -0500

    React setup for Admin tools

---
+++ WAFF.WebUI/Scripts/adminTools/AdminToolsContainer-compiled.js
@@ -0,0 +1,6 @@
+(function () {
+
+    window.AdminToolsContainer = React.createClass({});
+})(window.React);
+
+//# sourceMappingURL=AdminToolsContainer-compiled.js.map
---
+++ WAFF.WebUI/Scripts/adminTools/AdminToolsContainer.jsx
@@ -0,0 +1,5 @@
+(function(){
+
+    window.AdminToolsContainer = React.createClass({});
+
+})(window.React);
