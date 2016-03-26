ba0ba7335e0664e5233efdc232d18fd8df728ccf
Jeremiah Doody <n00810032@ospreys.unf.edu>
2016-03-26 14:38:40 -0400

    mobile service code started

---
+++ WAFF.Services/Mobile/MobileService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WAFF.DataAccess.Contexts;
+using System.Web.Mvc;
+
+namespace WAFF.Services.Mobile
+{
+    class MobileService
+    {
+
+        private readonly EFDbContext _dbContext = new EFDbContext();
+
+        public JsonResult GetAllFilmsByEventId(int id)
+        {
+        
+        
+        }
+
+    }
+}
---
+++ WAFF.WebUI/Controllers/MobileController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+
+namespace WAFF.WebUI.Controllers
+{
+    
+    public class MobileController : ApiController
+    {
+
+        //private MobileService _mobileService = new MobileService();
+
+        public JsonResult GetAllFilmsByEventId(int eventId)
+        {
+            //front door
+
+            var result =
+            //var result = Call Service passing along eventId
+
+            return;
+        }
+
+
+    }//end class
+
+}//end namespace
