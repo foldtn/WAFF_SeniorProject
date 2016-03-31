81fe2deee1c2661f422cbe9f7a4c09e8759a7492
Timothy Folds <n00830864@ospreys.unf.edu>
2016-03-31 12:30:17 -0400

    hopefully fixed stored procedure issue (getFilmsG and getGenres weren't showing up on GitHub)

---
+++ WAFF_dbProject/dbo/Stored Procedures/Reports/Voting Data/getGenres.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[getGenres]
+	@event int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT b.EventID, f.FilmGenre
+	FROM [FILMS] f
+	JOIN FILMBLOCKS fb
+	ON f.FilmID = fb.FilmID
+	JOIN BLOCKS b
+	ON b.BlockID = fb.BlockID
+	WHERE b.eventID = @event
+	GROUP BY b.EventID, f.FilmGenre
+END
