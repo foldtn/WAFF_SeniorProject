9d177088178b9058dab25d3088266c67204aca40
Timothy Folds <n00830864@unf.edu>
2016-05-09 17:27:22 -0400

    Modified database

--- WAFF_dbProject/dbo/tables/FILMS.sql
+++ WAFF_dbProject/dbo/tables/FILMS.sql
@@ -3,7 +3,7 @@
 	[FilmID] INT NOT NULL PRIMARY KEY identity, 
     [FilmName] VARCHAR(100) NOT NULL, 
     [FilmGenre] VARCHAR(30) NOT NULL, 
-    [FilmDesc] VARCHAR(250) NOT NULL,
+    [FilmDesc] VARCHAR(250) NULL,
 	[FilmLength] INT NOT NULL,
 	[FilmVotable] BIT NOT NULL DEFAULT 1
 )
--- WAFF_dbProject/dbo/tables/VOTERS.sql
+++ WAFF_dbProject/dbo/tables/VOTERS.sql
@@ -1,9 +1,12 @@
 ﻿CREATE TABLE [dbo].[VOTERS]
 (
-	[VoterID] INT NOT NULL PRIMARY KEY, 
+	[VoterID] INT NOT NULL, 
     [VoterAge] INT NULL, 
     [VoterEthnicity] VARCHAR(50) NULL, 
     [VoterEducation] VARCHAR(50) NULL, 
     [VoterIncome] MONEY NULL,
-	[VoterFirstTimer] BIT NOT NULL DEFAULT 0
+	[VoterFirstTimer] BIT NOT NULL DEFAULT 0,
+	[EventID] INT NOT NULL DEFAULT 1,
+	PRIMARY KEY (VoterID),
+	FOREIGN KEY (EventID) REFERENCES [EVENTS](EventID)
 )
